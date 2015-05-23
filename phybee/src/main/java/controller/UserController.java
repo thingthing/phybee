fe5d47d30467f7d5fe966981ccc79bae9ef25dad
Thing-leoh nicolas <nicolas1.thing-leoh@epitech.eu>
2015-05-23 17:13:44 +0800

    Spring security implemeted

--- phybee/src/main/java/controller/UserController.java
+++ phybee/src/main/java/controller/UserController.java
@@ -3,68 +3,91 @@ package controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import service.LoginValidator;
 import service.UserService;
 import bean.UserBean;
 
 @Controller
-@RequestMapping(value = "/login")
 public class UserController
 {
 	@Autowired
 	private UserBean user;
-	
+
 	@Autowired
 	private LoginValidator validator;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String showLogin(Map<String, Object> model)
+	@RequestMapping(value =
+	{ "/", "/home**" }, method = RequestMethod.GET)
+	public ModelAndView defaultPage()
 	{
-		System.out.println("Get login");
-		UserBean userForm = new UserBean();
-		model.put("login", userForm);
 
-		return "login";
+		ModelAndView model = new ModelAndView();
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = auth.getName(); // get logged in username
+
+		if (!name.equals("anonymousUser"))
+		{
+			UserBean userBean = new UserBean();
+			try
+			{
+				userBean = UserService.login(name);
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			user.setEmail(userBean.getEmail());
+			user.setId(userBean.getId());
+			user.setFirstName(userBean.getFirstName());
+			user.setLastName(userBean.getLastName());
+			user.setPassword(userBean.getPassword());
+			// for testing purpose:
+			System.out.println("username: " + user.getFirstName());
+			System.out.println("username: " + user.getLastName());
+			System.out.println("password: " + user.getPassword());
+			System.out.println("email: " + user.getEmail());
+
+			System.out.println("Id: " + user.getId());
+
+			model.addObject("username", name);
+		}
+		model.addObject("title", "Spring Security Password Encoder");
+		model.addObject("message", "This is default page!");
+		model.setViewName("home");
+		return model;
+
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String login(
-			@ModelAttribute("login") UserBean userBean, BindingResult result)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout)
 	{
-		//Validation code
-	    validator.validate(userBean, result);
-	     
-	    //Check validation errors
-	    if (result.hasErrors()) {
-	        return "login";
-	    }
-	    
-		try
-		{
-			userBean = UserService.login(userBean.getEmail(), userBean.getPassword());
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		user.setEmail(userBean.getEmail());
-		user.setId(userBean.getId());
-		user.setFirstName(userBean.getFirstName());
-		user.setLastName(userBean.getLastName());
-		user.setPassword(userBean.getPassword());
-		// for testing purpose:
-		System.out.println("username: " + user.getFirstName());
-		System.out.println("username: " + user.getLastName());
-		System.out.println("password: " + user.getPassword());
-		System.out.println("email: " + user.getEmail());
 
-		System.out.println("Id: " + user.getId());
-		return "redirect:reservation/movie";
+		ModelAndView model = new ModelAndView();
+		if (error != null)
+		{
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null)
+		{
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
 	}
 }
